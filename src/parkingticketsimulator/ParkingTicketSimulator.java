cc907045520645ff40dc18490873cd4890a04a9b
Ashika Shallow <slwashika@gmail.com>
2020-07-07 15:03:03 -0400

    Initial commit

---
+++ src/parkingticketsimulator/ParkingTicketSimulator.java
@@ -0,0 +1,17 @@
+
+package parkingticketsimulator;
+
+/**
+ * This program simulates a police officer issuing a parking ticket.
+ * @author Ashika Shallow
+ */
+public class ParkingTicketSimulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
