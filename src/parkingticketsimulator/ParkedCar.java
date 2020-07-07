6d9b1d143c75de932aa42b72dc546398df80e62f
Ashika Shallow <slwashika@gmail.com>
2020-07-07 16:06:08 -0400

    Initialised ParkedCar variables

---
+++ src/parkingticketsimulator/ParkedCar.java
@@ -0,0 +1,17 @@
+
+package parkingticketsimulator;
+
+/**
+ * The ParkedCar class simulates a parked car
+ * @author Ashika Shallow
+ */
+public class ParkedCar {
+    // Declare Variables
+    private String make = "";       // The car's make
+    private String model = "";      // The car's model
+    private String colour = "";     // The car's colour
+    private String license = "";    // The car's license number
+    private int minsParked;         // The number of minutes the car was parked
+    
+    
+}
