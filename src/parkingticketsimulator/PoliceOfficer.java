a0ec790f3bc4f5d39e64e42545e7251700f2836e
Ashika Shallow <slwashika@gmail.com>
2020-07-07 15:17:07 -0400

    Created the program's classes.

---
+++ src/parkingticketsimulator/PoliceOfficer.java
@@ -0,0 +1,10 @@
+
+package parkingticketsimulator;
+
+/**
+ * The PoliceOfficer class simulates a police officer inspecting parked cars.
+ * @author Ashika Shallow
+ */
+public class PoliceOfficer {
+    
+}
