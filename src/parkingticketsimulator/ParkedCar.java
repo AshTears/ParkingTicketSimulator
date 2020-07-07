2bb8815d88796ce22ebf24fa4e60db4a1977664b
Ashika Shallow <slwashika@gmail.com>
2020-07-07 17:01:18 -0400

    Added the class's methods.

--- src/parkingticketsimulator/ParkedCar.java
+++ src/parkingticketsimulator/ParkedCar.java
@@ -13,5 +13,71 @@ public class ParkedCar {
     private String license = "";    // The car's license number
     private int minsParked;         // The number of minutes the car was parked
     
+    /**
+     * This constructor initialises the car's details and the minutes it 
+     * was parked.
+     * @param mk The car's make
+     * @param mod The car's model
+     * @param col The car's colour
+     * @param lic The car's licence number
+     * @param mins Number of mins cars was parked
+     */
+    public ParkedCar(String mk, String mod, String col, String lic, int mins){
+        make = mk;
+        model = mod;
+        colour = col;
+        license = lic;
+        minsParked = mins;
+    }
     
+    /**
+     * getCarMake method
+     * @return The make of the car 
+     */
+    public String getCarMake(){
+        return make;
+    }
+    
+    /**
+     * getCarModel method
+     * @return The model of the car
+     */
+    public String getCarModel(){
+        return model;
+    }
+    
+    /**
+     * getCarColour method
+     * @return The colour of the car
+     */
+    public String getCarColour(){
+        return colour;
+    }
+    
+    /**
+     * getCarLicense method
+     * @return The car's license number
+     */
+    public String getCarLicense(){
+        return license;
+    }
+    
+    /**
+     * getMinsParked method
+     * @return Number of minutes the car was parked.
+     */
+    public int getMinsParked(){
+        return minsParked;
+    }
+    
+    /**
+     * toString method
+     * @return A string detailing the car's details.
+     */
+    public String toString(){
+        return "Car's make: " + make + 
+        "Car's model: " + model + 
+        "Car's colour: " + colour + 
+        "Car's license number: " + license;
+    }
 }
