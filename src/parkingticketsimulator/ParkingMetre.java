b7f6429a6030868670ee99732983143138762f1d
Ashika Shallow <slwashika@gmail.com>
2020-07-08 09:08:23 -0400

    Completed the ParkingTicket class.

--- src/parkingticketsimulator/ParkingMetre.java
+++ src/parkingticketsimulator/ParkingMetre.java
@@ -7,4 +7,31 @@ package parkingticketsimulator;
  */
 public class ParkingMetre {
     
+    // Declare variables
+    private int minsBought;     // Number of minutes of parking time bought.
+    
+    /**
+     * This constructor sets the value of the minsBought variable.
+     * @param mins Number of minutes bought.
+     */
+    ParkingMetre(int mins){
+        minsBought = mins;
+    }
+    
+    /**
+     * getMinsBought method
+     * @return The number of minutes bought.
+     */
+    public int getMinsBought(){
+        return minsBought;
+    }
+    
+    /**
+     * toString method
+     * @return A string detailing the minutes bought.
+     */
+    @Override
+    public String toString(){
+        return "Number of minutes bought: " + minsBought;
+    }
 }
