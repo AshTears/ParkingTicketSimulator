c74ebc25e177d43f06efbd17b52109da4805d6a1
Ashika Shallow <slwashika@gmail.com>
2020-07-08 09:24:04 -0400

    Added the constructor

--- src/parkingticketsimulator/ParkingTicket.java
+++ src/parkingticketsimulator/ParkingTicket.java
@@ -7,4 +7,16 @@ package parkingticketsimulator;
  */
 public class ParkingTicket {
     
+    private ParkedCar car;      // A ParkedCar object
+    private ParkingMetre metre; // A ParkingMetre object
+    private PoliceOfficer officer;  // A PoliceOfficer object
+    
+    ParkingTicket(ParkedCar car, ParkingMetre metre, PoliceOfficer off){
+        this.car = new ParkedCar(car.getCarMake(), car.getCarModel(),car.getCarColour(),
+        car.getCarLicense(),car.getMinsParked());
+        
+        this.metre = new ParkingMetre(metre.getMinsBought());
+        
+        officer = new PoliceOfficer();
+    }
 }
